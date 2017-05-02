5382e09504ff8d947f61170a53ce317536c4fbe5
pwwpche <pwwpche@gmail.com>
2017-05-02 14:55:44 -0700

    fseek is now working properly

--- ost-2.0.1/py/disk.c
+++ ost-2.0.1/py/disk.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 #include<errno.h> 
-
-#define SIZE 1
-#define NUMELEM 5
 
 int main(void)
 {
@@ -20,23 +18,31 @@ int main(void)
     char mode;
     int sector, read_size, disk_no;
     while(fscanf(spc_file, "%d,%d,%d,%c,%s", &disk_no, &sector, &read_size, &mode, &tmp_str) != EOF){
-        printf("%d, %d, %d, %c\n", disk_no, sector, read_size, mode);
-    }
-
-
-    if(0 != fseek(fd,11,SEEK_CUR))
-    {
-        printf("\n fseek() failed\n");
-        return 1;
-    }
-
-    char* buff = "12345";
-
-    int sizeWrite =  fwrite(buff,SIZE,strlen(buff),fd);
-    if(sizeWrite != 5)
-    {
-        printf("\n fwrite() failed %d \n", sizeWrite);
-        return 1;
+        
+	
+	if(disk_no != 0){
+	    continue;
+	}
+	printf("%d, %d, %d, %c\n", disk_no, sector, read_size, mode);
+	off_t offset = (off_t)(sector) * 512;
+	if(fseeko(fd, offset, SEEK_SET) == 0){
+            char* buff = malloc(read_size);
+	    if(mode == 'W'){
+		memset(buff, 0, read_size);
+		int size_write = fwrite(buff, 1, read_size, fd);
+		if(size_write != read_size){
+		    printf("\n fwrite() failed %d \n", size_write);
+		}
+	    }else if(mode == 'R'){
+		int size_read = fread (buff, 1, read_size, fd);
+		if(size_read != read_size){
+		    printf("\n fwrite() failed %d \n", size_read);
+		}
+	    }
+	    
+	}else{
+	    printf("fseek() failed, sector: %d, read_size: %d, mode: %c, offset: %ld \n", sector, read_size, mode, offset);
+	}
     }
 
     fclose(spc_file);
