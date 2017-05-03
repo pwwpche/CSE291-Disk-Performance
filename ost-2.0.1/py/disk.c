92cac976affae0ea463674f66195c9671c45ebea
root <xil564@ucsd.edu>
2017-05-02 20:25:55 -0700

    second update

--- ost-2.0.1/py/disk.c
+++ ost-2.0.1/py/disk.c
@@ -3,11 +3,13 @@
 #include<stdlib.h>
 #include<string.h>
 #include<errno.h> 
-#include<time.h>
+#include<sys/time.h>
+#include<unistd.h>
 int main(void)
 {
-    clock_t start,finish;
-    double tcost=0.0;
+    struct timeval start,finish;
+    double seekcost=0.0;
+    double readcost=0.0;
     int total=0;
     int flag;
     FILE* fd = NULL;
@@ -17,41 +19,36 @@ int main(void)
 	printf("fopen err %d \n", errno);
         return 1;
     }
-	start=clock();
-	finish=clock();
+	gettimeofday(&start,NULL);
     FILE* spc_file = fopen("sample.spc", "r");
     char tmp_str[100];
     char mode;
     int sector, read_size, disk_no;
     while(fscanf(spc_file, "%d,%d,%d,%c,%s", &disk_no, &sector, &read_size, &mode, &tmp_str) != EOF){
-        
-	
 	if(disk_no != 0){
 	    continue;
 	}
 	printf("%d, %d, %d, %c\n", disk_no, sector, read_size, mode);
 	off_t offset = (off_t)(sector) * 512;
-	start=clock();
+	gettimeofday(&start,NULL);
 	flag=fseeko(fd, offset, SEEK_SET);
-	finish=clock();
+	gettimeofday(&finish,NULL);
 	if(flag == 0){
             char* buff = malloc(read_size);
 	    if(mode == 'W'){
 		memset(buff, 0, read_size);
-		start=clock();
 		int size_write = fwrite(buff, 1, read_size, fd);
-		finish=clock();
 		//tcost=(double)(finish-start)/CLOCKS_PER_SEC;
-		printf("time cost of write %f\n",tcost);
+		//printf("time cost of write %f\n",tcost);
 		if(size_write != read_size){
 		    printf("\n fwrite() failed %d \n", size_write);
 		}
 	    }else if(mode == 'R'){
-		tcost+=(double)(finish-start);
-		start=clock();
+		seekcost+=(double)(finish.tv_sec-start.tv_sec+(finish.tv_usec-start.tv_usec)/1000000.0);
+		gettimeofday(&start,NULL);
 		int size_read = fread (buff, 1, read_size, fd);
-		finish=clock();
-		tcost+=(double)(finish-start);
+		gettimeofday(&finish,NULL);
+		readcost+=(double)(finish.tv_sec-start.tv_sec+(finish.tv_usec-start.tv_usec)/1000000.0);
 		total+=read_size;
 		//printf("time cost of read %f\n",tcost);
 		if(size_read != read_size){
@@ -66,7 +63,8 @@ int main(void)
 
     fclose(spc_file);
     fclose(fd);
-    printf("version 2 thoughput is %f MBPS \n", (double)total*CLOCKS_PER_SEC/(1024.0*1024.0*tcost));
+    printf("thoughput is %f MBPS \n", (double)total/(1024.0*1024.0*readcost));
+    printf("bandwidth is %f MBPS \n", (double)total/(1024.0*1024.0*(seekcost+readcost)));
 
     return 0;
 }
