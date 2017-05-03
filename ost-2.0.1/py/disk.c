1ba1d1d5ecb2c0ac72cf655882555c4a28d6e8a8
root <xil564@ucsd.edu>
2017-05-02 20:03:06 -0700

    disk update

--- ost-2.0.1/py/disk.c
+++ ost-2.0.1/py/disk.c
@@ -1,10 +1,15 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<errno.h> 
-
+#include<time.h>
 int main(void)
 {
+    clock_t start,finish;
+    double tcost=0.0;
+    int total=0;
+    int flag;
     FILE* fd = NULL;
     errno = 0;  
     fd = fopen("/dev/sdb","w+");
@@ -12,7 +17,8 @@ int main(void)
 	printf("fopen err %d \n", errno);
         return 1;
     }
-
+	start=clock();
+	finish=clock();
     FILE* spc_file = fopen("sample.spc", "r");
     char tmp_str[100];
     char mode;
@@ -25,16 +31,29 @@ int main(void)
 	}
 	printf("%d, %d, %d, %c\n", disk_no, sector, read_size, mode);
 	off_t offset = (off_t)(sector) * 512;
-	if(fseeko(fd, offset, SEEK_SET) == 0){
+	start=clock();
+	flag=fseeko(fd, offset, SEEK_SET);
+	finish=clock();
+	if(flag == 0){
             char* buff = malloc(read_size);
 	    if(mode == 'W'){
 		memset(buff, 0, read_size);
+		start=clock();
 		int size_write = fwrite(buff, 1, read_size, fd);
+		finish=clock();
+		//tcost=(double)(finish-start)/CLOCKS_PER_SEC;
+		printf("time cost of write %f\n",tcost);
 		if(size_write != read_size){
 		    printf("\n fwrite() failed %d \n", size_write);
 		}
 	    }else if(mode == 'R'){
+		tcost+=(double)(finish-start);
+		start=clock();
 		int size_read = fread (buff, 1, read_size, fd);
+		finish=clock();
+		tcost+=(double)(finish-start);
+		total+=read_size;
+		//printf("time cost of read %f\n",tcost);
 		if(size_read != read_size){
 		    printf("\n fwrite() failed %d \n", size_read);
 		}
@@ -47,7 +66,7 @@ int main(void)
 
     fclose(spc_file);
     fclose(fd);
-
+    printf("version 2 thoughput is %f MBPS \n", (double)total*CLOCKS_PER_SEC/(1024.0*1024.0*tcost));
 
     return 0;
 }
